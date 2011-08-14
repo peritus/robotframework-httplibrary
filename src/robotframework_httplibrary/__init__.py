2a8550db7ac8513d8bda3e7b98793bb2d1c2ad7e
Filip Noetzel <filip@j03.de>
2011-08-14 21:35:16 +0200

    Add some docstrings

--- src/robotframework_httplibrary/__init__.py
+++ src/robotframework_httplibrary/__init__.py
@@ -24,55 +24,101 @@ class HTTP:
     # setup
 
     def connect(self, host):
-        self._app = livetest.TestApp(host)
+        """
+        Sets the HTTP server for future requests. You must invoke this before
+        issuing any HTTP requests.
 
+        'host' is the name of the host, optionally with port (e.g. 'google.com' or 'localhost:5984')
+        """
+        self._app = livetest.TestApp(host)
 
     # request
 
     def GET(self, url):
+        """
+        Issues a HTTP GET request.
+
+        'url' is the URL relative to the server root, e.g. '/_utils/config.html'
+        """
         self._response = self.app.get(url, {}, self._request_headers)
         self._request_headers = {}
 
     def POST(self, url):
         self._response = self.app.post(url, {}, self._request_headers)
         self._request_headers = {}
+        """
+        Issues a HTTP POST request.
+
+        'url' is the URL relative to the server root, e.g. '/_utils/config.html'
+        """
 
     def follow_response(self):
+        """
+        Follows a HTTP redirect if the previous response status code was a 301 or 302.
+        """
         self._response = self.response.follow()
 
     # status code
 
     def response_should_succeed(self):
+        """
+        Fails if the response status code of the previous request was >= 400
+        """
         assert int(self.response.status[0:3]) < 400, \
                'Response should have been success, but was "%s"' % self.response.status
 
     def response_should_not_succeed(self):
+        """
+        Fails if the response status code of the previous request was < 400
+        """
         assert int(self.response.status[0:3]) > 399, \
                'Response should have been success, but was "%s"' % self.response.status
 
-    def response_status_code_should_equal(self, code):
-        assert self.response.status.startswith(code), \
-               '"%s" does not start with "%s"' % (self.response.status, code)
+    def response_status_code_should_equal(self, status_code):
+        """
+        Fails if the response status code was not the specified one.
 
-    def response_status_code_should_not_equal(self, code):
-        assert not self.response.status.startswith(code), \
-               '"%s" does start with "%s", but should not' % (self.response.status, code)
+        'status_code' the status code to compare against.
+        """
+        assert self.response.status.startswith(status_code), \
+               '"%s" does not start with "%s", but should have.' % (self.response.status, status_code)
+
+    def response_status_code_should_not_equal(self, status_code):
+        """
+        Fails if the response status code is equal to the one specified.
+
+        'status_code' the status code to compare against.
+        """
+        assert not self.response.status.startswith(status_code), \
+               '"%s" starts with "%s", but should not.' % (self.response.status, status_code)
 
     # response headers
 
     def response_should_have_header(self, header_name):
+        """
+        Fails if the response does not have a header named 'header_name'
+        """
         assert header_name in self.response.headers,\
                'Response did not have "%s" header, but should have' % header_name
 
     def response_should_not_have_header(self, header_name):
+        """
+        Fails if the response does has a header named 'header_name'
+        """
         assert not header_name in self.response.headers,\
                'Response did have "%s" header, but should not have' % header_name
 
     def get_response_header(self, header_name):
+        """
+        Get the response header with the name 'header_name'
+        """
         self.response_should_have_header(header_name)
         return self.response.headers[header_name]
 
     def response_header_should_equal(self, header_name, expected):
+        """
+        Fails if the value of response header 'header_name' does not equal 'expected'
+        """
         self.response_should_have_header(header_name)
         actual = self.response.headers[header_name]
         assert actual == expected,\
@@ -80,6 +126,9 @@ class HTTP:
                     header_name, expected, actual)
 
     def response_header_should_not_equal(self, header_name, not_expected):
+        """
+        Fails if the value of response header 'header_name' equals 'expected'
+        """
         self.response_should_have_header(header_name)
         actual = self.response.headers[header_name]
         assert actual != not_expected,\
@@ -89,9 +138,20 @@ class HTTP:
     # request headers
 
     def set_request_header(self, header_name, header_value):
+        """
+        Sets a request header for the next request.
+
+        'header_name' is the name of the header, e.g. 'User-Agent'
+        'header_value' is the key of the header, e.g. 'RobotFramework HttpLibrary (Mozilla/4.0)'
+        """
         self._request_headers[header_name] = header_value
 
     # debug
 
-    def show_response_in_browser(self, url):
+    def show_response_body_in_browser(self):
+        """
+        Opens your default web browser with the last request's response body.
+
+        This is meant for debugging response body's with complex media types.
+        """
         self._response.showbrowser()
