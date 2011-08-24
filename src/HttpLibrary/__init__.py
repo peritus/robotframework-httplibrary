bec25f494e5ea438e34a0544930d751de467451e
Filip Noetzel <filip@j03.de>
2011-08-24 16:07:37 +0200

    +Keyword "Json Value Should Be"

--- src/HttpLibrary/__init__.py
+++ src/HttpLibrary/__init__.py
@@ -292,6 +292,21 @@ class HTTP:
         """
         return jsonpointer.resolve_pointer(json_string, json_pointer)
 
+    def json_value_should_equal(self, json_string, json_pointer, expected_value):
+        """
+        Fails if the value of the target node of the JSON document
+        `json_string` specified by JSON Pointer `json_pointer` is not `expected_value`.
+
+        Example:
+        | Set Test Variable       | ${doc}  | {"foo": {"bar": [1,2,3]}} |             |
+        | Json Value Should Equal | ${doc}  | /foo/bar                  | "[1, 2, 3]" |
+        """
+
+        got = self.get_json_value(json_string, json_pointer)
+
+        assert got == expected_value, \
+               'JSON value "%s" does not equal "%s", but should have.' % (got, expected_value)
+
     @_with_json
     def set_json_value(self, json_string, json_pointer, json_value):
         """
