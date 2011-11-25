1d93f537f6239392b6b56a6ab0c3a7a89432cefa
Filip Noetzel <filip@j03.de>
2011-11-25 16:16:40 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Topic :: Software Development :: Testing
 
 setup(
   name             = 'robotframework-httplibrary',
-  version          = "0.1.1",
+  version          = "0.1.2",
   description      = 'Robot Framework wrapper for livetest',
   long_description = "Robot Framework wrapper for livetest",
   author           = 'Filip Noetzel',
