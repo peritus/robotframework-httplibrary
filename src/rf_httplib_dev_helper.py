0ac9f2497f738f81d8ae5ca09f1bb576c50eb922
Filip Noetzel <filip@j03.de>
2012-03-08 16:47:28 +0100

    buildout: use entry point that's rf 2.{6,7} independent

---
+++ src/rf_httplib_dev_helper.py
@@ -0,0 +1,13 @@
+
+def run_cli(args):
+    # abstracts away this change
+    # https://code.google.com/p/robotframework/source/detail?r=ebc6fbb542e6
+    # so we can test this lib against RF 2.6 and 2.7 w/o much config overhead
+
+    import robot
+    try:
+       robot.run_cli(args)
+    except Exception, e:
+       print e
+       import robot.runner
+       robot.run_from_cli(args, robot.runner.__doc__)
