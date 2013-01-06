a65b54c6785efb59cbb8f05122659051b3027e41
Filip Noetzel <filip@j03.de>
2013-01-06 14:38:22 +0100

    bump version

--- src/HttpLibrary/__init__.py
+++ src/HttpLibrary/__init__.py
@@ -32,7 +32,7 @@ class HTTP:
     Pointer, go to http://tools.ietf.org/html/draft-pbryan-zyp-json-pointer-00.
     """
 
-    ROBOT_LIBRARY_VERSION = "0.3.3"
+    ROBOT_LIBRARY_VERSION = "0.3.4"
 
     class Context(object):
         def __init__(self, http, host=None, scheme='http'):
