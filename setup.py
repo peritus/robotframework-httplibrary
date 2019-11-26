f7bffa6219fb06bc463b924b2ad2e292f75de099
Khizar Alam <khizar.alam@celeraone.com>
2019-11-26 10:29:38 +0100

    Bump version: 1.0.2 → 1.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = open(join(dirname(__file__), 'README.rst',)).read()
 
 setup(
     name='robotframework-httplibrary',
-    version="1.0.2",
+    version="1.1.0",
     description='Robot Framework keywords for HTTP requests',
     long_description=long_description,
     author='Filip Noetzel',
