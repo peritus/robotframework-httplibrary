6dae08f4e8bf1f149bea8dd531ff273085f562e3
shafeequ.ahmed <shafeequ.ahmed@celeraone.com>
2020-02-06 14:11:08 +0100

    Bump version: 1.1.0 → 1.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = open(join(dirname(__file__), 'README.rst',)).read()
 
 setup(
     name='robotframework-httplibrary',
-    version="1.1.0",
+    version="1.1.2",
     description='Robot Framework keywords for HTTP requests',
     long_description=long_description,
     author='Filip Noetzel',
