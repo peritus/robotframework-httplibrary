de2a799757fb6c98cedb337fbe4ca0012cc905ac
Dominik Kierzek <dominik.kierzek@celeraone.com>
2020-12-17 13:29:46 +0100

    Bump version: 1.1.4 → 1.1.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = open(join(dirname(__file__), 'README.rst',)).read()
 
 setup(
     name='robotframework-httplibrary',
-    version="1.1.4",
+    version="1.1.5",
     description='Robot Framework keywords for HTTP requests',
     long_description=long_description,
     author='Filip Noetzel',
